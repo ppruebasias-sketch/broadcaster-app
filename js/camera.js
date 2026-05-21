36cc83c2b653446f4b6b8b5070dc93b8bf3d229c
ppruebasias-sketch <ppruebasias@gmail.com>
2026-05-20 23:01:57 -0400

    Add files via upload

---
+++ js/camera.js
@@ -0,0 +1,4 @@
+// Archivo dedicado exclusivamente al manejo de hardware (Lentes y Micrófonos)
+console.log("Módulo de Cámara y Hardware: CARGADO");
+
+// Aquí iremos agregando (y nunca borrando) las funciones de captura
