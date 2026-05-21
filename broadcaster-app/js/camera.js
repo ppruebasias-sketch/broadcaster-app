044fa76b59aa38ef688f05ce6fdb953b9fd2f8bd
ppruebasias-sketch <ppruebasias@gmail.com>
2026-05-20 23:00:57 -0400

    Delete broadcaster-app directory

--- broadcaster-app/js/camera.js
+++
@@ -1,4 +0,0 @@
-// Archivo dedicado exclusivamente al manejo de hardware (Lentes y Micrófonos)
-console.log("Módulo de Cámara y Hardware: CARGADO");
-
-// Aquí iremos agregando (y nunca borrando) las funciones de captura
--- broadcaster-app/js/webrtc.js
+++
@@ -1,4 +0,0 @@
-// Archivo dedicado exclusivamente a la transmisión, redes y P2P
-console.log("Módulo de Transmisión WebRTC: CARGADO");
-
-// Aquí iremos agregando (y nunca borrando) la lógica de conexión con vMix/OBS
